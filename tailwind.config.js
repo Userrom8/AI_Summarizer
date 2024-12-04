96f4030585bd392d13a03e3586fb4b7e1f5ba05b
Romit <rombis02@gmail.com>
2024-12-04 18:43:37 +0530

    acknowledges history deletion

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         satoshi: ["Satoshi", "sans-serif"],
         inter: ["Inter", "sans-serif"],
-      }
+      },
+      keyframes: {
+        "slide-in-from-bottom": {
+          from: {
+            transform: "translateY(100%)",
+            opacity: "0",
+          },
+          to: {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+        },
+      },
+      animation: {
+        "slide-in-from-bottom": "slide-in-from-bottom .1s linear",
+      },
     },
   },
   plugins: [],
-}
+};
